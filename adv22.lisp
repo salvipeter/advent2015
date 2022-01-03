e15753cb00953cae76db734ae59adb80e0cbb19a
Peter Salvi <vukung@yahoo.com>
2022-01-03 23:55:20 +0100

    Refactoring

--- adv22.lisp
+++ adv22.lisp
@@ -9,27 +9,36 @@
         (incf (cdr (assoc item new)) delta))
     new))
 
-;;; Monolithic spaghetti
+(defun update-list (alist args)
+  (if (null args)
+      alist
+      (update-list (update alist (first args) (second args))
+                   (cddr args))))
+
+(defun cast-spell (spell state)
+  (let ((cost (cdr (assoc spell *spells*)))
+        (new-state (ecase spell
+                     (magic-missile (update state 'boss-hp -4))
+                     (drain (update-list state '(boss-hp -2 my-hp 2)))
+                     (shield (unless (> (cdr (assoc 'shield state)) 0)
+                               (update state 'shield 6)))
+                     (poison (unless (> (cdr (assoc 'poison state)) 0)
+                               (update state 'poison 6)))
+                     (recharge (unless (> (cdr (assoc 'recharge state)) 0)
+                                 (update state 'recharge 5))))))
+    (and new-state
+         (update-list new-state `(cost ,cost mana ,(- cost) spells ,spell)))))
+
 (defun adv22 (init hardp)
   (let (min)
-    (labels ((cast-spell (spell state)
-               (ecase spell
-                 (magic-missile (update state 'boss-hp -4))
-                 (drain (update (update state 'boss-hp -2) 'my-hp 2))
-                 (shield (unless (> (cdr (assoc 'shield state)) 0)
-                           (update state 'shield 6)))
-                 (poison (unless (> (cdr (assoc 'poison state)) 0)
-                           (update state 'poison 6)))
-                 (recharge (unless (> (cdr (assoc 'recharge state)) 0)
-                             (update state 'recharge 5)))))
-             (rec (state playerp)
+    (labels ((rec (state playerp)
                (let ((protected nil))
                  (when (and hardp playerp)
                    (setf state (update state 'my-hp -1)))
                  (when (> (cdr (assoc 'poison state)) 0)
-                   (setf state (update (update state 'poison -1) 'boss-hp -3)))
+                   (setf state (update-list state '(poison -1 boss-hp -3))))
                  (when (> (cdr (assoc 'recharge state)) 0)
-                   (setf state (update (update state 'recharge -1) 'mana 101)))
+                   (setf state (update-list state '(recharge -1 mana 101))))
                  (when (> (cdr (assoc 'shield state)) 0)
                    (setf state (update state 'shield -1)
                          protected t))
@@ -44,12 +53,7 @@
                         (loop with mana = (cdr (assoc 'mana state))
                               for (spell . cost) in *spells*
                               when (<= cost mana) do
-                                (let ((next (cast-spell
-                                             spell (update
-                                                    (update
-                                                     (update state 'cost cost)
-                                                     'mana (- cost))
-                                                    'spells spell))))
+                                (let ((next (cast-spell spell state)))
                                   (when next
                                     (rec next nil)))))
                        (t (rec (update state 'my-hp (if protected -1 -8)) t))))))
